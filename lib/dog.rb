9e9534f9043f1a0f2e1e97c952af194b62240a19
simk395 <simon.kong95@gmail.com>
2019-01-10 21:46:13 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+require "pry"
+class Dog
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all.each do |x|
+            puts x.name
+        end
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+end
